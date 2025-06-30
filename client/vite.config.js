580120928936a91a5f77389a57358652c7abffd1
Rabikiran67 <rabikiran1220@gmail.com>
2025-07-01 00:12:02 +0530

    Move vite to dependencies for production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // We REMOVE the server proxy section for production.
+  // The proxy is only for the local development server.
 });
